b6eedf19fa4bb5aaa1c6ac0c5c20f82e948d622c
Ravi Pankhaniya <pankhaniyaravi05@gmail.com>
2022-09-26 22:58:57 +0530

    Updates

---
+++ precache-manifest.9feb26b4c21a8e0cbd8ac0d923e462b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3585eba5c3df43234af2",
+    "url": "/TextUtils/static/css/main.16e45673.chunk.css"
+  },
+  {
+    "revision": "3585eba5c3df43234af2",
+    "url": "/TextUtils/static/js/main.3585eba5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "31c3e143c020eb3a4f9f071cb0c82d6e",
+    "url": "/TextUtils/index.html"
+  }
+];
